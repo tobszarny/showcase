d891ad4479208f27d67e7ad0cd22f720cc4fe129
Tomasz <tomasz.obszarny@gmail.com>
2019-03-04 11:42:23 +0100

    Cancellation first draft

--- reservation/src/main/java/com/flywithus/reservation/ReservationService.java
+++ reservation/src/main/java/com/flywithus/reservation/ReservationService.java
@@ -1,7 +1,31 @@
 package com.flywithus.reservation;
 
+import com.flywithus.flight.search.Flight;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReservationService {
+    void cancel(ReservationBasket basket) {
+        Optional<Flight> earliestDepartureFlight = basket.getTrips().stream()
+                .map(t -> t.getFlightsToDestination())
+                .flatMap(List::stream)
+                .min(Comparator.comparing(Flight::getDepartureTime));
+
+        if (earliestDepartureFlight.isPresent()) {
+            Flight flight = earliestDepartureFlight.get();
+            if (flight.getDepartureTime().isAfter(LocalDateTime.now().plus(5, ChronoUnit.DAYS))) {
+                preformCancel(basket);
+            }
+        }
+    }
+
+    private void preformCancel(ReservationBasket basket) {
+
+    }
 }
