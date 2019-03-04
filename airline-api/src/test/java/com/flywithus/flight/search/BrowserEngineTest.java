f1bde868afb01644abb430f44f77bccf2b93ae76
Tomasz <tomasz.obszarny@gmail.com>
2019-03-04 11:28:10 +0100

    Fixed persistence issues

--- airline-api/src/test/java/com/flywithus/flight/search/BrowserEngineTest.java
+++ airline-api/src/test/java/com/flywithus/flight/search/BrowserEngineTest.java
@@ -1,17 +1,23 @@
 package com.flywithus.flight.search;
 
+import com.flywithus.flight.search.configuration.AirlineJpaConfiguration;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
 
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.IsNot.not;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {TestsConfiguration.class})
+@ContextConfiguration(classes = {AirlineJpaConfiguration.class, TestsConfiguration.class})
+@AutoConfigureDataJpa
+@Transactional
 public class BrowserEngineTest {
 
     @Autowired
